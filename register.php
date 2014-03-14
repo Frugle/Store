57bba68937607c0e4d728c42123576034b6f7b5a
Iiro Surakka <iiro.surakka@gmail.com>
2014-03-14 10:53:38 +0200

    Add basic register html

---
+++ register.php
@@ -0,0 +1,34 @@
+<h1>Login</h1>
+<form method="post">
+	<label for="username">Username</label>
+	<input type="text" name="username">
+	<br>
+	<label for="password">Password</label>
+	<input type="text" name="password">
+	<br>
+	<label for="confirmpassword">Confirm password</label>
+	<input type="text" name="confirmpassword">
+	<br>
+	<label for="firstname">First name</label>
+	<input type="text" name="firstname">
+	<br>
+	<label for="lastname">Last name</label>
+	<input type="text" name="lastname">
+	<br>
+	<label for="address">Address</label>
+	<input type="text" name="address">
+	<br>
+	<label for="postcode">Post code</label>
+	<input type="text" name="postcode">
+	<br>
+	<label for="postoffice">Post office</label>
+	<input type="text" name="postoffice">
+	<br>
+	<label for="phone">Phone</label>
+	<input type="text" name="phone">
+	<br>
+	<label for="email">E-mail</label>
+	<input type="text" name="email">
+	<br>
+	<input type="submit" name="register" value="Register">
+</form>
