2ec6f8d7e9042d1f6efe0a68d7d2c9084b89a97f
Iiro Surakka <iiro.surakka@gmail.com>
2014-03-14 11:54:19 +0200

    Add basic user registeration

---
+++ include/db.php
@@ -0,0 +1,6 @@
+<?php
+	define("DB_HOST", "37.139.17.207");       //Database address
+	define("DB_USER", "webdev");            //Database username
+	define("DB_PASS", "nelio");          //Database password
+	define("DB_NAME", "store");       //Database name
+?>
--- register.php
+++ register.php
@@ -1,4 +1,75 @@
-<h1>Login</h1>
+<?php
+
+$post = $_POST;
+
+if (isset($post["register"]))
+{
+	require_once("include/db.php");
+
+	try
+	{
+		$db = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+		$query = "
+		INSERT INTO user (
+			usernameid, 
+			password, 
+			salt, 
+			firstname, 
+			lastname,
+			permissionlevel,
+			address,
+			postcode,
+			postoffice,
+			phone,
+			email)
+		VALUE (
+			:usernameid, 
+			:password, 
+			:salt, 
+			:firstname, 
+			:lastname,
+			:permissionlevel,
+			:address,
+			:postcode,
+			:postoffice,
+			:phone,
+			:email)
+		";
+
+		$prepare = $db->prepare($query);
+
+		$hash = password_hash($post["password"], PASSWORD_BCRYPT);
+
+		$permissionlevel = 0;
+
+		$prepare->bindParam(":usernameid", 		$post["username"]);
+		$prepare->bindParam(":password", 		$hash);
+		$prepare->bindParam(":salt", 			$hash);
+		$prepare->bindParam(":firstname", 		$post["firstname"]);
+		$prepare->bindParam(":lastname", 		$post["lastname"]);
+		$prepare->bindParam(":permissionlevel", $permissionlevel, PDO::PARAM_INT);
+		$prepare->bindParam(":address", 		$post["address"]);
+		$prepare->bindParam(":postcode", 		$post["postcode"]);
+		$prepare->bindParam(":postoffice", 		$post["postoffice"]);
+		$prepare->bindParam(":phone", 			$post["phone"]);
+		$prepare->bindParam(":email", 			$post["email"]);
+
+		$prepare->execute();
+
+		$db = null;
+	} 
+	catch (Exception $e) 
+	{
+		exit($e->getMessage());
+	}
+}
+
+?>
+
+
+<h1>Register</h1>
 <form method="post">
 	<label for="username">Username</label>
 	<input type="text" name="username">
