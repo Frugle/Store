f132a02c6e74d15659a649ea46414e40cab48514
Iiro <iiro.surakka@gmail.com>
2014-04-21 23:07:02 +0300

    Add footer also fuck css

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,50 @@
 	</div>
+	<footer>
+		<table>
+			<tr>
+				<td>
+					<h5>Aukiolo</h5>
+					<p>
+					ma-pe 09-18
+					<br>
+					la 12-18
+					<br>
+					su 12-17
+					</p>
+				</td>
+				<td>
+					<h5>Tuki</h5>
+					<p>
+					Sähköposti:
+					<a href="mailto:noreplysupport@store.com">noreply@store.com</a>
+					<br>
+					Puh.
+					040 123 4567
+					<br>
+					Arkisin klo. 09-15
+					</p>
+				</td>
+				<td>
+					<h5>Yhteystiedot</h5>
+					<p>
+					Osoite:
+					<br>
+					Store Oy
+					<br>
+					Paskakatu 10
+					<br>
+					40000 Jyväskylä
+					<br>
+					Sähköposti:
+					<a href="mailto:noreply@store.com">noreply@store.com</a>
+					<br>
+					Puh.
+					040 123 4567
+					</p>
+				</td>
+			</tr>
+		</table>
+	</footer>	
 </div>
 </body>
 </html>
