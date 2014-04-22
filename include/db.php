0573687a4934affc32227f83f1ef680008525bfa
Iiro <iiro.surakka@gmail.com>
2014-04-22 19:49:09 +0300

    Ignore config file

--- include/db.php
+++ include/db.php
@@ -1,8 +1,5 @@
 <?php
-	define("DB_HOST", "37.139.17.207");       //Database address
-	define("DB_USER", "webdev");            //Database username
-	define("DB_PASS", "nelio");          //Database password
-	define("DB_NAME", "store");       //Database name
+	require_once "config.php";
 
 	define("DEBUG", true);
 
